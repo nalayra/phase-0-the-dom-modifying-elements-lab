219e0b1cb3deaa3615d5499ed41e4d73e528d587
Nicholas Alayra <nalayra@hotmail.com>
2021-11-14 17:14:50 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//const element = document.createElement("div");
+const element = document.getElementById("main")
+element.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.textContent = "Nick is the champion"
+document.body.append(newHeader);
